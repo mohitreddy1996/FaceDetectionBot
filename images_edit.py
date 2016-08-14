1f419338f2f7b725d8980f2544f59d9091fe8efe
Mohit Reddy <mohitreddy1996@gmail.com>
2016-08-15 03:11:17 +0530

    Project Revamp

--- images_edit.py
+++
@@ -1,8 +0,0 @@
-from numpy import *
-import cv2
-
-img=cv2.imread('ronaldo.jpg',0)
-cv2.namedWindow('ronaldo.jpg',cv2.WINDOW_NORMAL)
-cv2.imshow('Ronaldo is great',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
